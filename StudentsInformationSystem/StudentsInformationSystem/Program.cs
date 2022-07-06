06be2aec61885ae3e628ba1aaba0e8ceff0c2a91
EmilijaSl <100716039+EmilijaSl@users.noreply.github.com>
2022-07-06 12:37:30 +0300

    Swish added I'm done now

--- StudentsInformationSystem/StudentsInformationSystem/Program.cs
+++ StudentsInformationSystem/StudentsInformationSystem/Program.cs
@@ -2,15 +2,74 @@
 
 var service = new Service();
 
-Task8StudentsFromSameLecture();
+ProgramIndeed();
+
+ void ProgramIndeed()
+{
+    Console.WriteLine("Duombaziu atsiskaitymo egzamino uzduotys");
+    Console.ReadLine();
+
+    bool repeat = true;
+    string userInput;
+    while (repeat)
+    { 
+        Console.Clear();
+        Console.WriteLine("[1] - Pirma uzduotis\n[2] - Antra uzduotis I.\n[3] - Antra uzduotis II\n[4] - Trecia uzduotis\n[5] - Ketvirta uzduotis\n[6] - Penkta uzduotis\n[7] - Sesta uzduotis\n[8] - Septinta uzduotis\n[9] - Astunta uzduotis\n[10] - Isjungti");
+        userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Console.Clear();
+                Task1CreateDepartmentWithAssignments();
+                break;
+            case "2":
+                Console.Clear();
+                Task2AssignLectureToDepartment();
+                break;
+            case "3":
+                Console.Clear();
+                Task2AsssignStudentToDepartment();
+                break;
+            case "4":
+                Console.Clear();
+                Task3CreateLectureAndAssignToDepartment();
+                break;
+            case "5":
+                Console.Clear();
+                Task4CreateStudentAddLecturesAndDepartment();
+                break;
+            case "6":
+                Console.Clear();
+                Task5MoveStudent();
+                break;
+            case "7":
+                Console.Clear();
+                Task6StudentsFromSameDepartment();
+                break;
+            case "8":
+                Console.Clear();
+                Task7LecturesFromSameDepartment();
+                break;
+            case "9":
+                Console.Clear();
+                Task8StudentsFromSameLecture();
+                break;
+            case "10":
+                Console.Clear();
+                repeat = false;
+                break;
+        }
+    }
+}
 
 
 void CreateDepartment()
 {
-    Console.WriteLine("Iveskite norimo sukurti Departamento pavadinima");
-    var name = Console.ReadLine();
-    service.CreateDepartment(name);
-    Console.WriteLine("Departamentas sukurtas");
+    
+        Console.WriteLine("Iveskite norimo sukurti Departamento pavadinima");
+        var name = Console.ReadLine();
+        service.CreateDepartment(name);
+        Console.WriteLine("Departamentas sukurtas, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");   
 }
 void CreateLecture()
 {
@@ -62,7 +121,7 @@ void Task1CreateDepartmentWithAssignments()
     bool repeat = true;
     while (repeat)
     {
-        Console.WriteLine("Iveskite norimo sukurti Departamento pavadinima");
+        Console.WriteLine("Iveskite norimos sukurti Katedros pavadinima");
         var name = Console.ReadLine();
         Console.Clear();
         Console.WriteLine("Iveskite paskaitos, kuria norite priskirti id");
@@ -75,7 +134,6 @@ void Task1CreateDepartmentWithAssignments()
         Console.Clear();
         service.CreateDepartmentWithStudentsAndLectures(name, lectId, studentId);
         Console.WriteLine("Katedra sukurta, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
@@ -88,7 +146,7 @@ void Task2AssignLectureToDepartment()
     bool repeat = true;
     while (repeat)
     {
-        Console.WriteLine("Iveskite katedros, kuriai norite priskirti paskaita id\n");
+        Console.WriteLine("Iveskite Katedros, kuriai norite priskirti paskaita id\n");
         service.GetDepartments();
         var depId = int.Parse(Console.ReadLine());
         Console.Clear();
@@ -98,20 +156,18 @@ void Task2AssignLectureToDepartment()
         Console.Clear();
         service.AssignLectureToDepartment(depId, lectId);
         Console.WriteLine("Paskaita priskirta, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
         }
     }
-
 }
 void Task2AsssignStudentToDepartment()
 {
     bool repeat = true;
     while (repeat)
     {
-        Console.WriteLine("Iveskite katedros, kuriai norite priskirti studenta id\n");
+        Console.WriteLine("Iveskite Katedros, kuriai norite priskirti studenta id\n");
         service.GetDepartments();
         var depId = int.Parse(Console.ReadLine());
         Console.Clear();
@@ -121,13 +177,11 @@ void Task2AsssignStudentToDepartment()
         Console.Clear();
         service.AssignStudentToDepartment(depId, studId);
         Console.WriteLine("Studentas pridetas, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
         }
     }
-
 }
 void Task3CreateLectureAndAssignToDepartment()
 {
@@ -143,13 +197,11 @@ void Task3CreateLectureAndAssignToDepartment()
         Console.Clear();
         service.CreateLectureAndAssignToDepartment(name, depId);
         Console.WriteLine("Paskaita sukurta, Katedra priskirta, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
         }
     }
-
 }
 void Task4CreateStudentAddLecturesAndDepartment()
 {
@@ -172,13 +224,11 @@ void Task4CreateStudentAddLecturesAndDepartment()
         Console.Clear();
         service.CreateStudentAddLecturesAndDepartment(name, lastName, departmentId, lectId);
         Console.WriteLine("Studentas sukurtas, informacija prideta, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
         }
     }
-
 }
 void Task5MoveStudent()
 {
@@ -195,7 +245,6 @@ void Task5MoveStudent()
         Console.Clear();
         service.MoveStudent(studId, depId);
         Console.WriteLine("Informacija atnaujinta, jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
-        Console.Clear();
         if (Console.ReadLine() == "0")
         {
             repeat = false;
@@ -207,7 +256,7 @@ void Task6StudentsFromSameDepartment()
     bool repeat = true;
     while (repeat)
     {
-        Console.WriteLine("Pasirinkite, kurio departamento studentus norite matyti?");
+        Console.WriteLine("Pasirinkite, kurios Katedros studentus norite matyti?");
         service.GetDepartments();
         var id = int.Parse(Console.ReadLine());
         var oneDepStudents = service.StudentsSameDepartment(id);
@@ -215,21 +264,19 @@ void Task6StudentsFromSameDepartment()
         {
             Console.WriteLine($"{student.Id}, {student.Name}, {student.LastName}\n");
         }
-        Console.Clear();
         Console.WriteLine("Jeigu norite baigti spauskite 0, jeigu norite testi - spauskite enter");
         if (Console.ReadLine() == "0")
         {
             repeat = false;
         }
     }
-
 }
 void Task7LecturesFromSameDepartment()
 {
     bool repeat = true;
     while (repeat)
     {
-        Console.WriteLine("Pasirinkite, kurio departamento paskaitas norite matyti?");
+        Console.WriteLine("Pasirinkite, kurios Katedros paskaitas norite matyti?");
         service.GetDepartments();
         var id = int.Parse(Console.ReadLine());
         Console.Clear();
